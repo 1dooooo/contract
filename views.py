887a6d96f341993c3c8d949b11a768d78fb6ab83
esunny <352387963@qq.com>
2018-08-05 18:11:47 -0700

    8.6 backup

--- views.py
+++ views.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 from FutureContract import InFutureContract, OutFutureContract
 from db_about import DBSession
 
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/admin', methods = ['GET', 'POST'])
+def admin():
+    return render_template('index.html')
+
+
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -36,13 +42,15 @@ def upload():
 
     return render_template('upload.html')
 
-@app.route('/', methods = ['GET', 'POST'])
-def index(variety = None):
 
+@app.route('/show', methods = ['GET', 'POST'])
+def show(variety = None):
     if request.args:
-        keys = ['郑商所', '大商所', '中金所', '上期所', '上期能源']
-        if request.args['location'] in keys:
-            contracts = DBSession().query(InFutureContract).filter(InFutureContract.product == request.args['variety']).first().to_dict()
+        variety = request.args['variety']
+        location = request.args['location']
+        in_column = [qo.exchange for qo in DBSession().query(InFutureContract, InFutureContract.exchange).distinct()]
+        if location in in_column:
+            contracts = DBSession().query(InFutureContract).filter(InFutureContract.product == variety, InFutureContract.exchange == location).first().to_dict()
         else:
             contracts = DBSession().query(OutFutureContract).filter(OutFutureContract.product == request.args['variety']).first().to_dict()
         return render_template('contract.html', contracts = contracts)
@@ -62,19 +70,8 @@ def index(variety = None):
             context['芝加哥商业交易所'].append(fc.product)
             continue
         context[fc.exchange].append(fc.product)
-
-    map_dict = {'上海国际能源交易中心':'上期能源', '上海期货交易所':'上期所', '郑州商品交易所':'郑商所','大连商品交易所':'大商所','中金所':'中金所','芝加哥商业交易所':'CME','欧洲期货交易所':'EUREX','香港交易所':'HKEX','美国洲际交易所':'ICE','伦敦金属交易所':'LME','新加坡交易所':'SGX','东京商品交易所':'TOCOM','马来西亚交易所':'MYX'}
-    tmp_dict = {}
-    for c in context:    
-        if c in map_dict:
-            tmp_dict[map_dict[c]] = context[c]
-        elif c == '':
-            tmp_dict['(未知所)'] = context[c]
-        else:
-            tmp_dict[c] = context[c]
-    context = tmp_dict
-
     return render_template('tabbar.html', context = context)
+
 
 @app.route('/add_in', methods=['POST', 'GET'])
 def add_in():
