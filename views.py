b6fdc385468631c780530bc0d362a20324cc1f26
esunny <352387963@qq.com>
2018-08-17 09:04:44 +0800

    m

--- views.py
+++ views.py
@@ -1,14 +1,62 @@
-from flask import Flask, render_template, request
-import collections
-import json
-import csv
-import os
-from urllib import parse
-
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Api, Resource
 from FutureContract import FutureContract
 from db_about import DBSession
+from urllib import parse
+
+import collections, json, csv, os
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class FcListApi(Resource):
+    def get(self):
+        context = collections.defaultdict(list)
+        fcs = DBSession().query(FutureContract).all()
+        for fc in fcs:
+            if fc.exchange.startswith('美国洲际交易所'):
+                context['美国洲际交易所'].append(fc.product)
+                continue
+            if fc.exchange.startswith('芝加哥商业交易所'):
+                context['芝加哥商业交易所'].append(fc.product)
+                continue
+            context[fc.exchange].append(fc.product)
+        return jsonify(context)
+    def post(self):
+        fc = FutureContract(**{key: request.form[key] for key in request.form})
+        session = DBSession()
+        session.add(fc)
+        session.commit()
+        session.close()
+        return {'ok': 1}
+
+class FcApi(Resource):
+    def get(self, var, loc):
+        session = DBSession()
+        contract = session.query(FutureContract).filter(FutureContract.product == var, FutureContract.exchange.like(loc+'%')).first()
+        session.close()
+        return contract.to_dict()
+    def post(self, var, loc):
+        session = DBSession()
+        fco = session.query(FutureContract).filter(FutureContract.product == var, FutureContract.exchange.like(loc+'%')).first()
+        fcn = FutureContract(**{key: request.form[key] for key in request.form})
+        session.delete(fco)
+        session.add(fcn)
+        session.commit()
+        session.close()
+        return {'ok': 1}
+    def delete(self, var, loc):
+        session = DBSession()
+        fc = session.query(FutureContract).filter(FutureContract.product == var, FutureContract.exchange.like(loc+'%')).first()
+        session.delete(fc)
+        session.commit()
+        session.close()
+        return {'ok':1}
+
+api.add_resource(FcListApi, '/FcList')
+api.add_resource(FcApi, '/Fc/<string:loc>/<string:var>')
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -39,38 +87,6 @@ def upload():
 
     return render_template('upload.html')
 
-@app.route('/admin', methods = ['GET', 'POST'])
-@app.route('/show', methods = ['GET', 'POST'])
-def show(variety = None):
-    if request.args:
-        variety = request.args['variety']
-        location = request.args['location']
-        contracts = DBSession().query(FutureContract).filter(FutureContract.product == variety, FutureContract.exchange.like(location+'%')).first().to_dict()
-        return render_template('contract.html', contracts = contracts)
-
-    context = collections.defaultdict(list)
-    fcs = DBSession().query(FutureContract, FutureContract.exchange, FutureContract.product)
-    for fc in fcs:
-        if fc.exchange.startswith('美国洲际交易所'):
-            context['美国洲际交易所'].append(fc.product)
-            continue
-        if fc.exchange.startswith('芝加哥商业交易所'):
-            context['芝加哥商业交易所'].append(fc.product)
-            continue
-        context[fc.exchange].append(fc.product)
-    return render_template(request.path + '.html', context = context)
-
-
-@app.route('/add', methods=['POST', 'GET'])
-def add():
-    if request.method == 'POST':
-        fc = FutureContract(**{key: request.form[key] for key in request.form})
-        session = DBSession()
-        session.add(fc)
-        session.commit()
-        session.close()
-    return render_template('add.html')
-
 @app.route('/search', methods=['POST'])
 def search():
     fcs = collections.defaultdict(list)
@@ -86,45 +102,16 @@ def search():
     return json.dumps(fcs)
 
 
-@app.route('/delete', methods=['POST'])
-def delete():
-    data_dict = {}
-    data = request.form['data']
-    data = parse.unquote(data)
-    lst = data.split('&')
-    for exp in lst:
-        e = exp.split('=')
-        data_dict[e[0]] = e[1]
-    product = data_dict['variety']
-    exchange = data_dict['location']
-    session = DBSession()
-    fc = session.query(FutureContract).filter(FutureContract.product == product, FutureContract.exchange.like(exchange+'%')).first()
-    session.delete(fc)
-    session.commit()
-    session.close()
-    return json.dumps({'ok':1})
-
-@app.route('/modify', methods=['POST','GET'])
+@app.route('/modify', methods=['GET'])
 def modify():
     product = request.args['variety']
     exchange = request.args['location']
-    global modify_fc
     session = DBSession()
     modify_fc = session.query(FutureContract).filter(FutureContract.product == product, FutureContract.exchange.like(exchange+'%')).first()
     session.commit()
     context = modify_fc.to_raw_dict()
     session.close()
-    return render_template('modify.html', context = context)
-
-@app.route('/update', methods=['POST'])
-def update():
-    fc = FutureContract(**{key: request.form[key] for key in request.form})
-    session = DBSession()
-    session.delete(modify_fc)
-    session.add(fc)
-    session.commit()
-    session.close()
-    return json.dumps({'OK':1})
+    return render_template('modify.html', contexts = context)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
