87398769421297b68e385431425bca7c29d2b5f2
1dooooo <352387963@qq.com>
2018-08-28 11:31:29 +0800

    Update db_about.py

--- db_about.py
+++ db_about.py
@@ -2,8 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from FutureContract import Base
 from urllib.parse import quote_plus as urlquote
-# engine = create_engine('postgresql://postgres:yisheng@192.168.23.180:5432/postgres')
-engine = create_engine('postgresql://root:%s@postgres-gqjljrlw.sql.tencentcdb.com:34160/cspcommodityf10' % urlquote("quote1234!@#$"))
+engine = create_engine('postgresql://postgres:yisheng@192.168.23.180:5432/postgres')
 DBSession = sessionmaker(bind=engine)
 
 if __name__ == '__main__':
